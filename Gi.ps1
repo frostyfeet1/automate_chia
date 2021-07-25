d58a310f4872413cd3987cc8d36a07ccfaa0ebd6
frostyfeet1 <78267937+frostyfeet1@users.noreply.github.com>
2021-07-25 18:06:03 +0200

    Create Gi.ps1

---
+++ Gi.ps1
@@ -0,0 +1,1 @@
+C:\Users\Ploter1\AppData\Local\chia-blockchain\app-1.1.6\resources\app.asar.unpacked\daemon\chia.exe plots create -k 32 -b 4256 -u 128 -r 3 -t G:\Temp1 -d R:\plotovi -n 1 -f <farmerkey> -p <poolkey>
